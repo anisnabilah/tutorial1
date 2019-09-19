1a05b80d49af654ad1139ec09b6bb3593d732ac1
anisnabilah <55278703+anisnabilah@users.noreply.github.com>
2019-09-19 17:53:07 +0800

    version1

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld{
+
+    public static void main(String[] args) {
+
+        int A = 3*6/2;
+        int B = 3*4/2;
+        int C = 4*3;
+
+        System.out.println("Hello world Malaysia STIA1113");
+        System.out.println("A=  " + 0.5*3*6 );
+        System.out.println("B=  " + 0.5*3*4 );
+        System.out.println("C=  " + 4*3 );
+        System.out.println("ABC=  " + (A+B+C) );
+
+    }
+
+}
